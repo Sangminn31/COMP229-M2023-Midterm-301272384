da7ec7fcf50920f01ac12368e952f877f9e10ba7
Sangminn31 <sanguuu123@gmail.com>
2023-07-05 13:59:26 -0400

    first
    dd

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+   url: "mongodb+srv://sangu123:2jGELFyip2UD5865@cluster0.fu0dcfn.mongodb.net/DressStore?retryWrites=true&w=majority"
+  };
